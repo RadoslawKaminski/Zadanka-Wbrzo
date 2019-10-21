3337c969749ee4a04c0cd1ff678d22cdfecf7ccb
Radosław Kamiński <radziowojak@gmail.com>
2019-10-21 22:54:15 +0200

    jakby zrobiono header

--- index.php
+++ index.php
@@ -29,22 +29,28 @@ if(isset($_SESSION['error']['server']))
 		<meta name="keywords" content="">
 
 		<link rel="stylesheet" href="css/main.css" type="text/css">
+		<link rel="stylesheet" href="css/header.css" type="text/css">
+		<link rel="stylesheet" href="css/index.css" type="text/css">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
-		<nav>
-			<ul>
-				<li><a href="galeria.php">Galeria</a></li>
-				<li><a href="dodaj-album.php">Załóź album</a></li>
-				<li><a href="dodaj-foto.php">Dodaj zdjęcie</a></li>
-				<li><a href="top-foto.php">Najlepiej oceniane</a></li>
-				<li><a href="nowe-foto.php">Najnowsze</a></li>
-				<li><a href="index.php">Zaloguj się</a></li>
-				<li><a href="index.php">Rejestracja</a></li>
-			<ul>
-		</nav>
-    	<h1>Zaloguj się lub zarejestruj</h1>
+    	<header>
+    		<div id="header_text_wrapper">
+    			<h1>Zaloguj się lub zarejestruj</h1>
+    		</div>
+			<nav>
+				<ul>
+					<li><a href="galeria.php">Galeria</a></li>
+					<li><a href="dodaj-album.php">Załóź album</a></li>
+					<li><a href="dodaj-foto.php">Dodaj zdjęcie</a></li>
+					<li><a href="top-foto.php">Najlepiej oceniane</a></li>
+					<li><a href="nowe-foto.php">Najnowsze</a></li>
+					<li><a href="index.php">Zaloguj się</a></li>
+					<li><a href="index.php">Rejestracja</a></li>
+				<ul>
+			</nav>
+		</header>
 		<h3 style="display: <?=$server_e_display?>" class='error'><?=$server_error?></h3>
 		<form id="zar_form" action="rejestracja.php" method="post">
 			<input class="login" name="login" type="text" placeholder="Wpisz login" value="<?=(isset($_SESSION['zar_login']) ? $_SESSION['zar_login'] : "")?>" required>
