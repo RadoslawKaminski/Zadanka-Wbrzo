d446f920355c41c1903e5da4a86ff873f3ed57c7
Radosław Kamiński <radziowojak@gmail.com>
2019-11-18 12:50:51 +0100

    rozpoczęto dodawanie zdjęcia

--- dodaj-foto.php
+++ dodaj-foto.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true)
     <meta name="keywords" content="">
 
     <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/dodaj-foto.css" type="text/css">
 	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -36,9 +37,68 @@ if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true)
 		</nav>
     </header>
     <main>
+		<nav id="albumy">
+			<h3>Albumy</h3>
+			<ul>
+			<?php
+				require_once 'database.php';
+				if($albumQuery = $mysqli->prepare('SELECT id, tytul, data, id_uzytkownika FROM albumy WHERE id_uzytkownika = ?'))
+				{
+					$albumQuery->bind_param("i", $_SESSION['user_id']);
+					$albumQuery->execute();
+					
+					$result = $albumQuery->get_result();
+					$albumQuery->close();
+
+					while($album = $result->fetch_assoc()) 
+					{
+						echo "<li class='".($_SESSION['selected_album'] == $album['id'] ? "current" : "")."' album_id='".$album['id']."'>".$album['tytul']."</li>";
+					}
+				}
+			?>
+			</ul>
+		</nav>
+		<section>
+			<h3 style="display: none" class='error'></h3>
+			<div class="form-wrapper">
+				<form id="foto_form" action="dodaj-foto.php" method="post">
+					
+					<input type="file" name=""><br>
+					<span style="display: none" class='error'></span>
+					<input class="dodaj-album" name="dodaj-album" type="submit" value="Dodaj album">
+				</form>
+			</div>
+		</section>
     </main>
     <footer>
         Radosław Kamiński 4Tb
-    </footer>
+	</footer>
+	<script>
+			$("#foto-form").submit(function() 
+			{
+				var formData = new FormData();
+				let file_n = $(this).find('.file-input')[0].files[0]['name'].replace(/\s/g, "-");
+				formData.append(file_n, $(this).find('.file-input')[0].files[0]);
+				$.ajax(
+				{
+					type: 'POST',
+					url: 'dodaj-foto.php',
+					data: formData,
+					processData: false,  // tell jQuery not to process the data
+					contentType: false,  // tell jQuery not to set contentType
+					success: function(data) 
+					{
+						if(data == "no_errors")
+						{
+							
+						}
+						else
+						{
+							$('#errors').html(data);
+						}
+					}
+				});
+			}
+	</script>
 </body>
 </html>
