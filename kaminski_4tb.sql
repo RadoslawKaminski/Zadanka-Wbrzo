d0167300384215c91f9f448d965948fe8e360844
Radosław <radoslaw.kaminski.mail@gmail.com>
2019-11-29 02:25:16 +0100

    dodano przykładowe dana / backup bazy

--- kaminski_4tb.sql
+++ kaminski_4tb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 21 Paź 2019, 23:57
+-- Czas generowania: 29 Lis 2019, 02:24
 -- Wersja serwera: 10.3.15-MariaDB
 -- Wersja PHP: 7.3.6
 
@@ -34,6 +34,34 @@ CREATE TABLE `albumy` (
   `data` datetime NOT NULL,
   `id_uzytkownika` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `albumy`
+--
+
+INSERT INTO `albumy` (`id`, `tytul`, `data`, `id_uzytkownika`) VALUES
+(6, 'album 1', '2019-11-28 17:16:30', 18),
+(7, 'przykładowy album 2', '2019-11-28 17:16:40', 18),
+(8, 'album 3', '2019-11-29 00:49:14', 18),
+(9, 'kolejny album nr 4', '2019-11-29 00:49:26', 18),
+(10, 'album innego usera 1', '2019-11-29 00:51:02', 19),
+(11, 'album ASDFasdf1234 nr 2', '2019-11-29 00:51:27', 19),
+(12, 'album 7', '2019-11-29 01:12:29', 19),
+(13, 'album 8', '2019-11-29 01:12:34', 19),
+(14, 'album 9', '2019-11-29 01:12:39', 19),
+(15, 'album 10', '2019-11-29 01:12:49', 19),
+(16, 'album 11', '2019-11-29 01:13:00', 19),
+(17, 'album 12', '2019-11-29 01:13:03', 19),
+(18, 'album 13', '2019-11-29 01:13:08', 19),
+(19, 'album 14', '2019-11-29 01:13:13', 19),
+(20, 'album 15', '2019-11-29 01:13:17', 19),
+(21, 'album 16', '2019-11-29 01:13:21', 19),
+(22, 'album 17', '2019-11-29 01:13:27', 19),
+(23, 'album 18', '2019-11-29 01:13:32', 19),
+(24, 'album 19', '2019-11-29 01:13:35', 19),
+(25, 'album 20', '2019-11-29 01:22:15', 18),
+(26, 'album 21', '2019-11-29 01:22:21', 18),
+(27, 'album 22', '2019-11-29 01:22:26', 18);
 
 -- --------------------------------------------------------
 
@@ -72,6 +100,40 @@ CREATE TABLE `zdjecia` (
   `data` datetime NOT NULL,
   `zaakceptowane` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `zdjecia`
+--
+
+INSERT INTO `zdjecia` (`id`, `opis`, `id_albumu`, `data`, `zaakceptowane`) VALUES
+(49, 'zdjęcie 1 w albumie 1', 6, '2019-11-28 17:17:42', 1),
+(50, 'pionowe zdjęcie 2 w albumie 1', 6, '2019-11-28 17:18:03', 0),
+(51, 'zdjęcie 1 w albumie 2', 7, '2019-11-28 17:18:27', 1),
+(52, 'zdjęcie 2 w albumie 2', 7, '2019-11-28 17:18:39', 1),
+(53, '', 8, '2019-11-29 00:49:52', 0),
+(54, '', 8, '2019-11-29 00:49:59', 1),
+(55, '', 9, '2019-11-29 00:50:08', 0),
+(56, '', 9, '2019-11-29 00:50:16', 1),
+(57, '', 9, '2019-11-29 00:50:28', 1),
+(58, '', 11, '2019-11-29 00:51:45', 1),
+(59, '', 11, '2019-11-29 00:51:50', 1),
+(60, '', 10, '2019-11-29 00:52:01', 1),
+(61, '', 25, '2019-11-29 01:22:39', 1),
+(62, '', 26, '2019-11-29 01:22:44', 1),
+(63, '', 27, '2019-11-29 01:22:49', 1),
+(64, '', 12, '2019-11-29 01:23:10', 1),
+(65, '', 13, '2019-11-29 01:23:15', 1),
+(66, '', 14, '2019-11-29 01:23:20', 1),
+(67, '', 15, '2019-11-29 01:23:24', 1),
+(68, '', 16, '2019-11-29 01:23:29', 1),
+(69, '', 17, '2019-11-29 01:23:33', 1),
+(70, '', 18, '2019-11-29 01:23:37', 1),
+(71, '', 19, '2019-11-29 01:23:42', 1),
+(72, '', 20, '2019-11-29 01:23:46', 1),
+(73, '', 21, '2019-11-29 01:24:43', 1),
+(74, '', 22, '2019-11-29 01:24:48', 1),
+(75, '', 23, '2019-11-29 01:24:54', 1),
+(76, '', 24, '2019-11-29 01:24:59', 1);
 
 -- --------------------------------------------------------
 
@@ -145,7 +207,7 @@ ALTER TABLE `zdjecia_oceny`
 -- AUTO_INCREMENT dla tabeli `albumy`
 --
 ALTER TABLE `albumy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
@@ -157,7 +219,7 @@ ALTER TABLE `uzytkownicy`
 -- AUTO_INCREMENT dla tabeli `zdjecia`
 --
 ALTER TABLE `zdjecia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- Ograniczenia dla zrzutów tabel
