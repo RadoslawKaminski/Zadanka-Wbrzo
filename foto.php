3f724106a0d751bc78bb2fdf9bcf2ddb044d3e20
Radosław Kamiński <radziowojak@gmail.com>
2019-11-29 12:21:02 +0100

    rozpoczęcie zadania 7

--- foto.php
+++ foto.php
@@ -4,19 +4,19 @@ session_start();
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-    <title>Album | Projekt aplikacji webowej</title>
+    <title>Zdjęcie | Projekt aplikacji webowej</title>
     <meta name="description" content="Strona główna projektu aplikacji webowej">
     <meta name="keywords" content="">
 
     <link rel="stylesheet" href="css/main.css" type="text/css">
-    <link rel="stylesheet" href="css/galeria.css" type="text/css">
+    <link rel="stylesheet" href="css/zdjecie.css" type="text/css">
     <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <header>
         <div id="header_text_wrapper">
-            <h1>Album</h1>
+            <h1>Zdjęcie</h1>
         </div>
         <nav>
             <ul>
@@ -34,52 +34,60 @@ session_start();
         </nav>
     </header>
     <main id="container">
-        <a class='go-galery' href="galeria.php">Powrót do galerii</a><br>
         <?php
             function filter($get_name) {return htmlspecialchars(filter_input(INPUT_GET, $get_name), ENT_QUOTES, 'UTF-8');}
 
-            $strona = isset($_GET['strona']) ? filter("strona") : 1;
             $album_id = filter("album_id");
+            $zdjecie_id = filter("zdjecie_id");
+            
+            echo "<a class='go-galery' href='album.php?album_id=$album_id'>Powrót do listy zdjęć</a><br>";
             
             $ok = true;
 
-            if(!(ctype_digit($strona) || is_int($strona)))
-                $ok = false;
             if(!(ctype_digit($album_id) || is_int($album_id)))
+                $ok = false;
+            if(!(ctype_digit($zdjecie_id) || is_int($zdjecie_id)))
                 $ok = false;
 
             if($ok)
             {
                 require_once 'database.php';
-
-                $pomin = ($strona * 20) - 20;
                 
                 if($zdjecieQuery = $mysqli->prepare("SELECT 
-                                                            z.id,
+                                                            z.id AS id,
                                                             z.id_albumu,
-                                                            count(z.id) AS countId
+                                                            z.data,
+                                                            z.opis,
+                                                            z.zaakceptowane,
+                                                            a.id AS a_id,
+                                                            a.tytul,
+                                                            a.id_uzytkownika,
+                                                            u.id AS u_id,
+                                                            u.login,
+                                                            count(o.id_zdjecia) AS l_ocen,
+                                                            sum(o.ocena)/count(o.id_zdjecia) AS ocena
                                                         FROM zdjecia z
-                                                        WHERE z.id_albumu = ?
-                                                        GROUP BY z.id
-                                                        ORDER BY z.id DESC
-                                                        LIMIT $pomin, 18446744073709551615"))
+                                                            join albumy a
+                                                                ON a.id = z.id_albumu
+                                                            join uzytkownicy u
+                                                                ON a.id_uzytkownika = u.id
+                                                            left outer join zdjecia_oceny o
+                                                                ON o.id_zdjecia = z.id
+                                                                    AND o.id_uzytkownika = u.id
+                                                        WHERE z.id = ?
+                                                        GROUP BY z.id"))
                 {
                     require_once 'class.img.php';
 
-                    $zdjecieQuery->bind_param("i", $album_id);
+                    $zdjecieQuery->bind_param("i", $zdjecie_id);
                     $zdjecieQuery->execute();
                     
                     $result = $zdjecieQuery->get_result();
 
-                    $ile_stron = (int)floor((($result->num_rows)+$pomin)/20)+1;
-
-                    $wyswietlono_ilosc = 0;
-                    while(($zdjecie = $result->fetch_assoc()))
+                    $zdjecie = $result->fetch_assoc();
+                    if($zdjecie)
                     {
-                        if($wyswietlono_ilosc >= 20)
-                            break;
                         $img = new Image("img/".$zdjecie['id_albumu']."/".$zdjecie['id']);
-                        $img->SetMaxSize(180);
 
                         ob_start();
                         $img->Send();
@@ -87,33 +95,18 @@ session_start();
                         ob_end_clean();
 
                         echo    "<section class='zdjecie' zdjecie_id='".$zdjecie['id']."'>
-                                    <a href='foto.php?album_id=$album_id&zdjecie_id=".$zdjecie['id']."'>
+                                    <a href='foto.php?zdjecie_id=".$zdjecie['id']."'>
                                         <img 
                                             src='data:image/jpeg;base64, $output' 
                                             alt='Miniatura zdjęcia w albumie'
                                         >
                                     </a>
                                 </section>";
-                        $wyswietlono_ilosc++;
-                    }
-                    if($ile_stron > 1 || $strona > 1)
-                    {?>
-                                <nav id='paginacja'>
-                                    <ul>
-                    <?php
-                        for($i=1; $i<=($ile_stron); $i++)
-                        {
-                            echo "<li><a href='?album_id=$album_id&strona=$i' ".($i == $strona ? "class='current-page'" : "").">$i</a></li>";
-                        }
-                    ?>                
-                                    </ul>
-                                </nav>
-                    <?php
                     }
                 }
             }
+            echo "<br><a class='go-galery' href='album.php?album_id=$album_id'>Powrót do listy zdjęć</a>";
         ?>
-        <br><a class='go-galery' href="galeria.php">Powrót do galerii</a>
     </main>
     <footer>
         Radosław Kamiński 4Tb
